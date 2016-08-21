3d67ec093628ddb025fd5a64f3416b6514efe3ab
Shao Yu Lung <bestlong168@gmail.com>
2016-08-21 15:18:04 +0800

    test rename

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -2,7 +2,7 @@
 
 class ModelTest extends PHPUnit_Framework_TestCase
 {
-    public function testConstruct()
+    public function testSetValueByConstruct()
     {
         $model = new \Bestlong\Database\Model(['name' => 'Allen']);
         $this->assertEquals('Allen', $model->name);
